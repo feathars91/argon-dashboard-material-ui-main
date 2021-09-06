ec75bf5c348e0d30280563f0ecd334e2c38b6d74
Feadars <abelino.chinchilla@gmail.com>
2021-09-06 15:10:50 -0500

    idk1

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require("gulp");
 const gap = require("gulp-append-prepend");
 
-gulp.task('dev', gulp.series('clean'));
 
 gulp.task("licenses", async function () {
   // this is to add Creative Tim licenses in the production mode for the minified js
